ed0faac3d5ffdd370905677dce132bfc29cefbf5
dngthanhvy <dng.thanhvy@gmail.com>
2021-11-16 00:05:25 +0100

    add: isogram

---
+++ isogram/Isogram.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        return word.ToLower().Where(c => char.IsLetter(c)).GroupBy(c => c).All(g => g.Count() == 1);
+    }
+}
