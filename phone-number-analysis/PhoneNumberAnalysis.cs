9a1c043bf5bd994cfabac325f3e86967351f2291
dngthanhvy <dng.thanhvy@gmail.com>
2021-11-16 00:05:48 +0100

    add: phone number analysis

---
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -0,0 +1,13 @@
+using System;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber) {
+        string[] numbers = phoneNumber.Split('-');
+        return (numbers[0].Equals("212"), numbers[1].Equals("555"), numbers[2]);
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) {
+        return phoneNumberInfo.IsFake;
+    }
+}
