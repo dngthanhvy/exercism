c092bb26ac694a0d148e02ad7fc5fb6cd199eb55
dngthanhvy <dng.thanhvy@gmail.com>
2021-11-16 00:05:35 +0100

    add: pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        return input.ToLower().Where(c => char.IsLetter(c)).Distinct().Count() == 26;
+    }
+}
